a01dac0b580fcbbccb0fc2952484b6ebabd7d208
Diego Arenas <dtarenas@gmail.com>
2024-03-16 11:52:43 -0500

    Add Owner controller, Fix owner property datatypes

---
+++ Veterinary/Veterinary.API/Controllers/OwnerController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Veterinary.API.Data;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Controllers
+{
+    [ApiController]
+    [Route("/api/owners")]
+    public class OwnerController : ControllerBase
+    {
+        private readonly VeterinaryDbContext _context;
+
+        public OwnerController(VeterinaryDbContext context)
+        {
+            _context = context;
+        }
+
+        //Metodo Get por lista--- Select * From Owners
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Owners.ToListAsync());
+        }
+
+        //Metodo post' Guardar registros
+        [HttpPost]
+        public async Task<ActionResult> Post(Owner owner)
+        {
+            _context.Owners.Add(owner);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        //Metodo Get por parametro' Select * From Owners
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var owner = await _context.Owners.FirstOrDefaultAsync();
+            if (owner == null)
+            {
+                return NotFound(owner); //404
+            }
+
+            return Ok(owner);
+        }
+
+        //Metodo Put - Actualizar registros
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Owner owner)
+        {
+            _context.Owners.Update(owner);
+            await _context.SaveChangesAsync();
+            return Ok(owner);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var filasafectadas = await _context.Owners.Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+            if (filasafectadas == 0)
+            {
+                return NotFound(); //404
+            }
+
+            return NoContent(); //204
+        }
+    }
+}
--- Veterinary/Veterinary.Shared/Entities/Owner.cs
+++ Veterinary/Veterinary.Shared/Entities/Owner.cs
@@ -1,15 +1,44 @@
-﻿namespace Veterinary.Shared.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veterinary.Shared.Entities
 {
     public class Owner
     {
         public int Id { get; set; }
+
+        [Display(Name = "Primer Nombre")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(50, ErrorMessage = "No puede superar los 50 digitos")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(50, ErrorMessage = "No puede superar los 50 digitos")]
         public string LastName { get; set; }
+
+        [Display(Name = "Documento")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(13, ErrorMessage = "No puede superar los 50 digitos")]
         public string Document { get; set; }
+
+        [Display(Name = "Correo eletronico")]
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(255, ErrorMessage = "No puede superar los 50 digitos")]
+        [EmailAddress(ErrorMessage = "Formato invalido")]
         public string EmailAddress { get; set; }
+
+        [Display(Name = "Telefono")]
+        [MaxLength(10, ErrorMessage = "No puede superar los 10 digitos")]
         public string FixedPhone { get; set; }
+
+        [Display(Name = "Telefono Movil")]
+        [MaxLength(10, ErrorMessage = "No puede superar los 10 digitos")]
         public string ContactPhone { get; set; }
+
+        [Display(Name = "Direccion de residencia")]
+        [MaxLength(10, ErrorMessage = "No puede superar los 10 digitos")]
         public string Address { get; set; }
+
         public string FullName => $"{FirstName} {LastName}";
     }
 }
